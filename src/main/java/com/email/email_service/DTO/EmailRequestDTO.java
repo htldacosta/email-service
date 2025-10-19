cedcf7afaef2878df768fd1931f63937375d06a9
htldacosta <hitaludcosta@gmail.com>
2025-10-18 22:34:49 -0400

    tratamento de exceptions foi criado

--- src/main/java/com/email/email_service/DTO/EmailRequestDTO.java
+++ src/main/java/com/email/email_service/DTO/EmailRequestDTO.java
@@ -9,20 +9,20 @@ import lombok.Data;
 @Data
 public class EmailRequestDTO {
 
-    @NotBlank(message = "O campo 'nome não pode ser vazio.")
+    @NotBlank(message = "O campo 'Nome não pode ser vazio.")
     @Size(max = 40, message = "O campo 'nome' deve ter no máximo 40 caracteres")
     private String name;
 
-    @NotBlank(message = "O campo 'email' não pode ser vazio.")
+    @NotBlank(message = "O campo 'E-mail' não pode ser vazio.")
     @Email(message = "O formato do 'email' é inválido.")
     @Size(max = 60, message = "O campo 'email' deve ter no máximo 60 caracteres.")
     private String email;
 
-    @NotBlank(message = "O campo 'subject' não pode ser vazio.")
+    @NotBlank(message = "O campo 'Assunto' não pode ser vazio.")
     @Size(max = 40, message = "O campo 'subject' deve ter no máximo 40 caracteres.")
     private String subject;
 
-    @NotBlank(message = "O campo 'message' não pode ser vazio.")
+    @NotBlank(message = "O campo 'Mensagem' não pode ser vazio.")
     @Size(max = 800, message = "O campo 'message' deve ter no máximo 800 caracteres.")
     private String message;
 }
