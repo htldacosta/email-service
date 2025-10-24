93ba9cd8fe3e10e16f197a9ef68eea1a96d66831
htldacosta <hitaludcosta@gmail.com>
2025-10-24 17:47:40 -0400

    testes uniOtarios

--- src/main/java/com/email/email_service/config/ApiKeyInterceptor.java
+++ src/main/java/com/email/email_service/config/ApiKeyInterceptor.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 public class ApiKeyInterceptor implements HandlerInterceptor {
 
-    @Value("${app.api.key}")
+    @Value("${app.api-key}")
     private String apiKey;
 
 
